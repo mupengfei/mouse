d41d0ed794dfd8da9639a36845270449f4caa7da
mupf <mupf.zj@gmail.com>
2016-11-09 18:54:42 +0800

    1

---
+++ src/main/java/com/mrgan/mouse/Application.java
@@ -0,0 +1,22 @@
+package com.mrgan.mouse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+
+@SpringBootApplication
+public class Application implements EmbeddedServletContainerCustomizer {
+	private static Logger logger = LogManager.getLogger(Application.class
+			.getName());
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class);
+	}
+
+	public void customize(ConfigurableEmbeddedServletContainer container) {
+		container.setPort(2046);
+	}
+}
